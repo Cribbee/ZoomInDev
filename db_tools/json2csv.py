531021a2c657a9ae991b18557c304672ebf2a33a
cribbee <cribbee66@gmail.com>
2018-05-28 20:54:52 +0800

    dbtools for "json2csv"

--- db_tools/json2csv.py
+++ db_tools/json2csv.py
@@ -1,46 +1,27 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Cribbee'
-__create_at__ = 2018 / 5 / 15
+__create_at__ = 2018 / 5 / 28
 
-import csv
 import json
-import sys
 import codecs
+import csv
 
 
 def trans():
-    jsonData = codecs.open('/Users/cribbee/Downloads/raw.2.json', 'r', 'utf-8')
-    # csvfile = open(path+'.csv', 'w') # 此处这样写会导致写出来的文件会有空行
-    # csvfile = open(path+'.csv', 'wb') # python2下
-    data = {}
-    keys_write = True
-    csvfile = codecs.open('/Users/cribbee/Downloads/转换完成1.csv', 'w', 'utf-8')  # python3下
-    writer = csv.writer(csvfile)
 
-    for line in jsonData:  # 获取属性列表
-        dic = json.loads(line[0:-1])
-        keys = dic.keys()
-        break
+    fr = codecs.open("/Users/cribbee/Downloads/raw.2.json", 'r', 'utf-8')
+    ls = json.load(fr)
+    data = [list(ls[0].keys())]
+    for item in ls:
+        print(item)
+        data.append(list(item.values()))
+    fr.close()
 
-    for dic in jsonData:  # 读取json数据的每一行，将values数据一次一行的写入csv中
-
-        print(dic)
-        dic = json.loads(dic[0:])
-
-        for key in keys:
-            if key in dic:
-                data[key] = dic[key]
-            else:
-                data[key] = ""
-        print(data)
-
-        if keys_write == True:
-            writer.writerow(data.keys())
-        writer.writerow(data.values())
-        keys_write = False
-
-    jsonData.close()
-    csvfile.close()
+    fw = codecs.open("/Users/cribbee/Downloads/转换完成1.csv", 'w', 'utf-8')
+    writer = csv.writer(fw)
+    for item in data:
+        writer.writerow(item)
+    fw.close()
 
 
 if __name__ == '__main__':
