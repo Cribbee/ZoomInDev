8ea4e0aaef9a3c0476e38699683e072466ac1bb0
cribbee <cribbee66@gmail.com>
2018-10-10 15:51:50 +0800

    type：fix scope：admin.py subject:  i want to fix the bug with df’s index Body： fix the bug by

--- apps/data_mining/admin.py
+++ apps/data_mining/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 
 # Register your models here.
 # 测试github新分支 request
+# 测试github新分支 request# 测试github新分支 request# 测试github新分支 request# 测试github新分支 request# 测试github新分支 request
