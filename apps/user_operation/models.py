f807f25c390440573cf741994a67f249620d9c79
cribbee <cribbee66@gmail.com>
2018-11-05 16:47:25 +0800

    type：feature scope：user_operation.models.py & user_operation.serializers.py & urls.py & user_operation.views.py subject:  prepare to build feature of publish Body： create model of publish and continue

--- apps/user_operation/models.py
+++ apps/user_operation/models.py
@@ -10,7 +10,7 @@ User = get_user_model()
 
 class UserTask(models.Model):
     """
-    用户操作任务
+    用户收藏任务
     """
     user = models.ForeignKey(User, models.CASCADE, null=True, verbose_name="用户", )
     tasks = models.ForeignKey(TaskInfo, models.CASCADE, verbose_name="任务", help_text="任务id")
@@ -24,3 +24,27 @@ class UserTask(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Publish(models.Model):
+    """
+    用户发布任务
+    """
+
+    owner = models.ForeignKey(User, models.CASCADE, null=True, verbose_name="分享者用户")
+    user = models.ForeignKey(User, models.CASCADE, null=True, verbose_name="被分享者用户")
+    source_task = models.ForeignKey(TaskInfo, models.CASCADE, verbose_name="源任务", help_text="源任务id")
+    task = models.ForeignKey(TaskInfo, models.CASCADE, default=-1, verbose_name="源任务", help_text="源任务id")
+    task_name = models.CharField(max_length=80, null=True, blank=True, verbose_name="分享任务名", help_text="分享任务名")
+    add_time = models.DateTimeField(null=True, blank=True, default=datetime.now, verbose_name=u"添加时间")
+    update_time = models.DateTimeField(null=True, blank=True, default=datetime.now, verbose_name=u"更新时间")
+
+    class Meta:
+        verbose_name = '发布'
+        verbose_name_plural = verbose_name
+        #联合字段构成唯一集合，避免重复收藏
+        unique_together = ("owner", "task_name")
+
+    def __str__(self):
+        return self.task_name
+
--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -3,10 +3,13 @@ from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.response import Response
+from rest_framework import status
 
 from utils.permissions import IsOwnerOrReadOnly
-from .models import UserTask
-from .serializers import UserTaskSerializer
+from .models import UserTask, Publish
+from tasks.models import TaskInfo, DataSet, Chart
+from .serializers import UserTaskSerializer, PublishSerializer
 
 
 class UserTaskViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin,
@@ -39,6 +42,48 @@ class UserTaskViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Ret
             return UserTaskSerializer
         elif self.action == "create":
             return UserTaskSerializer
+
+        return UserTaskSerializer
+
+
+class PublishViewset(viewsets.ModelViewSet):
+    """
+    list:
+        获取用户收藏列表
+    retrieve:
+        判断某个商品是否已经收藏
+    create:
+        收藏任务
+    """
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    serializer_class = PublishSerializer
+
+    def get_queryset(self):
+        return Publish.objects.filter(owner=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        path = "/home/ZoomInDataSet/"  # 服务器路径
+        source_task = TaskInfo.objects.get(id=serializer.data["source_task"])
+        task = TaskInfo.objects.create(user=serializer.data["user"], task_name=serializer.data["task_name"],
+                                       data_num=source_task.data_num, task_desc=source_task.task_desc,)
+        task.task_folder = path + str(task.id)
+        task.save()
+        # 遍历source_task 对应的dataset、chart、data_mining各个实体，复制其路径下文件，同时创建记录
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return UserTaskSerializer
+        elif self.action == "create":
+            return PublishSerializer
 
         return UserTaskSerializer
 
