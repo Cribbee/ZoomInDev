1af6798142482ff7ad759d343ef1e9e9c2a6f95d
LEONllllsu <sudongao@163.com>
2018-11-11 21:44:50 +0800

    scope:user_operation.views subject:add new feature body: 完善逻辑代码框架

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -1,21 +1,27 @@
+import shutil
+from datetime import datetime
+
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
 
 from utils.permissions import IsOwnerOrReadOnly
 from .models import UserTask, Publish
 from . import tests
 from tasks.models import TaskInfo, DataSet, Chart
+from users.models import UserProfile
 from data_mining.models import Clustering, Regression
-from .serializers import UserTaskSerializer, PublishSerializer, PublishDetailSerializer
+from .serializers import UserTaskSerializer, PublishSerializer, PublishDetailSerializer, SummarySerializer
 
 
 class UserTaskViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin,
-                     mixins.DestroyModelMixin, viewsets.GenericViewSet):
+                      mixins.DestroyModelMixin, viewsets.GenericViewSet):
     """
     list:
         获取用户收藏列表
@@ -60,7 +66,6 @@ class PublishViewset(viewsets.ModelViewSet):
         return Publish.objects.filter(user=self.request.user)
 
     def create(self, request, *args, **kwargs):
-
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -74,7 +79,7 @@ class PublishViewset(viewsets.ModelViewSet):
         task = TaskInfo.objects.create(user_id=serializer.data["shared_user"], task_name=serializer.data["task_name"],
                                        data_num=source_task.data_num, task_desc=source_task.task_desc)
 
-        #新建任务后完善publish表
+        # 新建任务后完善publish表
         Publish_model = Publish.objects.get(id=serializer.data["id"])
         Publish_model.task = task.id
         Publish_model.save()
@@ -114,14 +119,35 @@ class PublishViewset(viewsets.ModelViewSet):
             for s in Regression.objects.filter(data_set=i.id):
                 s.id = None
                 s.data_set = i.id
-                s.chart_folder1 = tests.trans(k.chart_folder1, str(task.id))
-                s.chart_folder2 = tests.trans(k.chart_folder2, str(task.id))
+                s.chart_folder1 = tests.trans(s.chart_folder1, str(task.id))
+                s.chart_folder2 = tests.trans(s.chart_folder2, str(task.id))
                 s.add_time = datetime.now()
                 s.updated_time = None
                 s.user_id = task.user_id
                 s.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(detail=False, methods=['POST'])
+    def publish(self, request, pk=None):
+        result = {}
+        user = UserProfile.objects.get(id=request.user)
+        result['user_name'] = user.username
+        task = TaskInfo.objects.get(id=request.data['task_id'])
+        result['add_time'] = task.add_time
+        data_set = DataSet.objects.filter(task_id=request.data['task_id'])
+        for i in data_set:
+            result['data_set_name'] = i.title
+            for k in Clustering.objects.filter(data_set=i.id):
+                result['data_mining_clustering1'] = k.chart_folder1
+                result['data_mining_clustering2'] = k.chart_folder2
+            for s in Regression.objects.filter(data_set=i.id):
+                result['data_mining_regression1'] = s.chart_folder1
+                result['data_mining_regression2'] = s.chart_folder2
+
+        # 再加上 结论表
+
+        return Response(result, status=status.HTTP_200_OK)
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -130,6 +156,34 @@ class PublishViewset(viewsets.ModelViewSet):
             return PublishSerializer
         elif self.action == "retrieve":
             return PublishDetailSerializer
+        elif self.action == "shared":
+            return PublishSerializer
 
         return PublishSerializer
 
+
+class SummaryViewset(viewsets.ModelViewSet):
+    """
+
+    """
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
+    serializer_class = SummarySerializer
+
+    def get_queryset(self):
+        return Summary.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return UserTaskSerializer
+        elif self.action == "create":
+            return SummarySerializer
+
+        return SummarySerializer
